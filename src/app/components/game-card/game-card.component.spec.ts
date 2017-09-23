2c4ee65416c3712c7fbc6d97979e15562adc5efc
Mike <sanders.michael.j@gmail.com>
2017-09-23 19:31:38 -0400

    trying to deploy to heroku

--- src/app/components/game-card/game-card.component.spec.ts
+++ src/app/components/game-card/game-card.component.spec.ts
@@ -3,8 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameCardComponent } from './game-card.component';
 
 describe('GameCardComponent', () => {
-  let component: GameCardComponent;
   let fixture: ComponentFixture<GameCardComponent>;
+  let component: GameCardComponent;
+  let app;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,6 +17,7 @@ describe('GameCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GameCardComponent);
     component = fixture.componentInstance;
+    app = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
@@ -23,7 +25,20 @@ describe('GameCardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should have a table for the game squares', () => {
-    expect(fixture.debugElement.nativeElement.querySelector('#game-card')).toBeTruthy();
+  it('should have a table for the game squares', () => {
+    expect(app.querySelector('#game-card')).toBeTruthy();
+  });
+
+  xit('should display the correct name for the given square', () => {
+    const squares = {
+      '1': [{'name': 'Super Man'},{'name': 'Wonder Woman'},{'name': 'Aquaman'},{'name': 'Robin'},{'name': 'Darth Vader'}],
+      '2': [{'name': 'Batman'},{'name': 'Gumby'},{'name': 'Iron Man'},{'name': 'Thor'},{'name': 'Bodhi Rook'}],
+      '3': [{'name': 'Deadpool'},{'name': 'Storm'},{'name': 'CENTER'},{'name': 'Black Widow'},{'name': 'Luke Skywalker'}],
+      '4': [{'name': 'Logan'},{'name': 'Darth Maul'},{'name': 'Venom'},{'name': 'Nightwing'},{'name': 'Groot'}],
+      '5': [{'name': 'Woverine'},{'name': 'Link'},{'name': 'Captain America'},{'name': 'Ren'},{'name': 'Frodo'}]
+    };
+    component.squares = squares;
+    fixture.detectChanges();
+    // expect(app.querySelector())
   });
 });
